a48be44bcbbad25fd54eb9a3eefd06c54e1b9504
Kapil Sharma <113497837+KapilSharmaCodingId@users.noreply.github.com>
2022-09-29 18:39:30 +0530

    Update keypadCode.java

--- Work 2/keypadCode.java
+++ Work 2/keypadCode.java
@@ -1,3 +1,28 @@
+/*
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+Return empty string for numbers 0 and 1.
+Note : 1. The order of strings are not important.
+2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
+Input Format :
+Integer n
+Output Format :
+All possible strings in different lines
+Constraints :
+1 <= n <= 10^6
+ Input:
+23
+ Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+*/
+
 public class keypadCode {
 
 	// Return a string array that contains all the possible strings
