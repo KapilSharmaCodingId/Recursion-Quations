7a7d5ecb3d905763c43b2cfe7853b944b4bc873b
Kapil Sharma <113497837+KapilSharmaCodingId@users.noreply.github.com>
2022-09-30 05:31:35 +0530

    Add files via upload

---
+++ Work 2/PrintSubsequenceOfString.java
@@ -0,0 +1,24 @@
+public class PrintSubsequenceOfString {
+
+    public static void findSubsequence(String input,String output){
+
+        if(input.length() == 0){
+            System.out.print(output + " ");
+            return ;
+        }
+        
+        //lets assum first element is not occure  
+        findSubsequence(input.substring(1), output);
+        //lets assum first element is  occure 
+        findSubsequence(input.substring(1), output + input.charAt(0));
+    }
+
+    public static void PrintSubsequence(String input){
+
+        findSubsequence(input,"");
+    }
+	public static void main(String args[]){
+        String input  = "xyz";
+         PrintSubsequence(input);
+    }
+}
